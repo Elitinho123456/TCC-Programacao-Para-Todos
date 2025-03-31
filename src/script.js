f01bf2f31259daa3d851f13286e26cd86e5612c8
Elytus <elitinho2502@hotmail.com>
2025-03-31 19:03:19 -0300

    Fix some bugs
    Definition of a maximum size for the 'box';
    Application of a stable collision system;
    More design for the Character;
    Objective added (Flag).

--- src/script.js
+++ src/script.js
@@ -2,36 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Player
     const player = document.querySelector('.player');
-    const parede = document.querySelector('.parede');
+    const parede = document.querySelector('.desafio');
+
 
     // Função: Verificação Player colidiu ou não com a Parede e Reinicio Automatico.
     let loop = setInterval(() => {
 
         function iniciarLoop() {
             loop = setInterval(() => {
+
                 const playerPosition = player.offsetLeft; // Pega a posição do player
                 const paredePostion = parede.offsetLeft; // Pega a posição da parede
-    
-                if (playerPosition >= (paredePostion)) { // Caso o player esteja dentro do px designado, executa:
+                const paredetop = parede.offsetTop;
 
-                    // Seta a animação para nenhuma, "reseta o personagem"
-                    player.style.animation = 'none';
-                    // Seta o Player para a posição onde colidiu com a "Parede"
-                    player.style.left = `${playerPosition}px`;
-    
-                    player.style.animation = 'game-over 1s ease-out';
-                    player.style.bottom = '-80px';
-    
-                    // Troca a cor do personagem para fins visuais
-                    player.src = './images/Level-1 ref-Super-Mario/playerT.png';
-    
-                    // Encerra a verificação
-                    clearInterval(loop);
-    
-                    // Adiciona o evento 'animationend'
-                    player.addEventListener('animationend', resetPlayer);
+                if ((paredetop) < 520) {
+                    if (playerPosition >= (paredePostion - (player.width - 120))) { // Caso o player esteja dentro do px designado, executa:
 
+                        console.log(`Player morto = ${playerPosition} e parede ${paredePostion}`)
+
+                        // Seta a animação para nenhuma, "reseta o personagem"
+                        player.style.animation = 'none';
+                        // Seta o Player para a posição onde colidiu com a "Parede"
+                        player.style.left = `${playerPosition}px`;
+
+                        player.style.animation = 'game-over 1s ease-out';
+                        player.style.bottom = '-80px';
+
+                        // Troca a cor do personagem para fins visuais
+                        player.src = './images/Level-1 ref-Super-Mario/playerT.png';
+
+                        // Encerra a verificação
+                        clearInterval(loop);
+
+                        // Adiciona o evento 'animationend'
+                        player.addEventListener('animationend', resetPlayer);
+
+                    }
                 }
+
 
             }, 10);
 
@@ -43,16 +51,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Remove o 'animationend' para evitar múltiplas chamadas
             player.removeEventListener('animationend', resetPlayer);
-    
+
             // Redefine a posição e o estilo do player
 
-            player.style.left = '-3%'; 
+            player.style.left = '-3%';
             player.style.bottom = '60px';
             player.style.width = '250px';
             player.style.height = '100px';
             player.src = './images/Level-1 ref-Super-Mario/playerT.gif'; // volta a animação
             player.style.animation = ''; // Remove a animação 'game-over'
-    
+
             // Reinicia o loop de verificação de colisão
             iniciarLoop();
 
